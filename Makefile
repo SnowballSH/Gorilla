cdd203efdd3c4e8c2d2fda7f0845f8e59bb9ff49
SnowballSH <derek070211@gmail.com>
2021-01-20 23:04:50 -0600

    init

--- Makefile
+++
@@ -1,8 +0,0 @@
-build:
-	@go build ./gorilla
-
-prod:
-	@go build -ldflags "-s -w" ./gorilla
-
-api:
-	@go build  -o ./api ./gorilla/api
--- gorilla/config/config_test.go
+++
@@ -1,9 +0,0 @@
-package config
-
-import "testing"
-
-func TestBuiltins(t *testing.T) {
-	if GetOSNewline("WINDOWS") != OSNEWLINES["windows"] {
-		t.Fatalf("windows not match")
-	}
-}
