8a254a10c9aabed4f3ac4fb62f214c67bb7406d2
SnowballSH <derek070211@gmail.com>
2021-03-31 16:29:37 -0500

    main.go flag parsing

--- go.mod
+++ go.mod
@@ -4,7 +4,8 @@ go 1.16
 
 require (
 	ekyu.moe/leb128 v0.0.0-20190626180622-d3722dc409a8
-	github.com/c-bata/go-prompt v0.2.6 // indirect
-	github.com/fatih/color v1.10.0 // indirect
+	github.com/c-bata/go-prompt v0.2.6
+	github.com/fatih/color v1.10.0
 	github.com/stretchr/testify v1.7.0
+	golang.org/x/sys v0.0.0-20210320140829-1e4c9ba3b0c4 // indirect
 )
--- main.go
+++ main.go
@@ -1,7 +1,42 @@
 package main
 
-import "github.com/SnowballSH/Gorilla/repl"
+import (
+	"flag"
+	"github.com/SnowballSH/Gorilla/exports"
+	"github.com/SnowballSH/Gorilla/repl"
+	"github.com/fatih/color"
+	"io/ioutil"
+	"os"
+	"strings"
+)
 
 func main() {
-	repl.Start()
+	flag.Parse()
+	if flag.NArg() == 0 {
+		repl.Start()
+	} else {
+		fn := flag.Arg(0)
+		content, err := ioutil.ReadFile(fn)
+
+		if err != nil {
+			color.Red(err.Error())
+			os.Exit(1)
+		}
+
+		code := strings.TrimSpace(string(content))
+
+		bc, err := exports.CompileGorilla(code)
+		if err != nil {
+			color.Red(err.Error())
+			os.Exit(1)
+		}
+
+		_, _, err = exports.ExecuteGorillaBytecodeFromSource(bc, code)
+
+		if err != nil {
+			color.Red(err.Error())
+			os.Exit(1)
+		}
+	}
+	os.Exit(0)
 }
