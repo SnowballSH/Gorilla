e1a1a062fc26c9b82db874d029654b658513476c
SnowballSH <derek070211@gmail.com>
2021-04-27 13:54:43 -0500

    fix integer division by 0

--- src/integer.rs
+++ src/integer.rs
@@ -78,6 +78,9 @@ fn div<'a>(this: BaseObject<'a>, args: Vec<BaseObject<'a>>) -> ObjResult<'a> {
             let g = inner!(this.internal_value, if NativeFunction);
                 return Err(format!("{} expects an integer", g.0))
             });
+            if b == 0 {
+                return Err(format!("Integer division by 0 in {} / {}", a, b))
+            }
             Ok(new_integer(a / b))
         }
         None => {
@@ -98,6 +101,9 @@ fn mod_<'a>(this: BaseObject<'a>, args: Vec<BaseObject<'a>>) -> ObjResult<'a> {
             let g = inner!(this.internal_value, if NativeFunction);
                 return Err(format!("{} expects an integer", g.0))
             });
+            if b == 0 {
+                return Err(format!("Integer modulo by 0 in {} % {}", a, b))
+            }
             Ok(new_integer(a % b))
         }
         None => {
@@ -192,11 +198,47 @@ mod tests {
     use crate::integer::new_integer;
 
     #[test]
-    fn example() {
+    fn binop() {
+        let ii = new_integer(10);
+        let mut f = ii.instance_get("+".to_string()).unwrap();
+        f.set_parent(ii);
+        let res = f.clone().call(f, vec![new_integer(1)]);
+        assert_eq!(res.unwrap().to_string(), "11");
+
         let ii = new_integer(10);
         let mut f = ii.instance_get("-".to_string()).unwrap();
         f.set_parent(ii);
         let res = f.clone().call(f, vec![new_integer(1)]);
-        assert_eq!(res.unwrap().to_string(), "9")
+        assert_eq!(res.unwrap().to_string(), "9");
+
+        let ii = new_integer(10);
+        let mut f = ii.instance_get("*".to_string()).unwrap();
+        f.set_parent(ii);
+        let res = f.clone().call(f, vec![new_integer(-10)]);
+        assert_eq!(res.unwrap().to_string(), "-100");
+
+        let ii = new_integer(10);
+        let mut f = ii.instance_get("/".to_string()).unwrap();
+        f.set_parent(ii);
+        let res = f.clone().call(f, vec![new_integer(2)]);
+        assert_eq!(res.unwrap().to_string(), "5");
+
+        let ii = new_integer(10);
+        let mut f = ii.instance_get("%".to_string()).unwrap();
+        f.set_parent(ii);
+        let res = f.clone().call(f, vec![new_integer(3)]);
+        assert_eq!(res.unwrap().to_string(), "1");
+
+        let ii = new_integer(10);
+        let mut f = ii.instance_get("/".to_string()).unwrap();
+        f.set_parent(ii);
+        let res = f.clone().call(f, vec![new_integer(0)]);
+        assert!(res.is_err());
+
+        let ii = new_integer(10);
+        let mut f = ii.instance_get("%".to_string()).unwrap();
+        f.set_parent(ii);
+        let res = f.clone().call(f, vec![new_integer(0)]);
+        assert!(res.is_err());
     }
 }
---
+++ src/lib.rs
@@ -0,0 +1,7 @@
+pub mod obj;
+pub mod env;
+pub mod integer;
+pub mod helper;
+pub mod vm;
+pub mod grammar;
+pub mod native_function;
